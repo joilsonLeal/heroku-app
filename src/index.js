0ed42ad76c0f1828cb627a3354f3539d885ef2cf
joilson <joilson_rg@hotmail.com>
2020-11-12 21:31:30 -0300

    chore: configure nodemon and dotenv

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: '.env' });
 
 const app = express();
 
@@ -6,4 +9,4 @@ app.get('/', (req, res) => {
     return res.json({hello: 'world'});
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
