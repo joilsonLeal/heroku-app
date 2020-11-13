9822bfc5154b5ca81d78dad2167bcfcdb1712a84
joilson <joilson_rg@hotmail.com>
2020-11-12 21:46:33 -0300

    chore: change start script, add node_env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: '.env' });
+if(process.env.NODE_ENV)
+    dotenv.config({ path: '.env' });
 
 const app = express();
 
