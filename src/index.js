737b418a36bd4976da702a61b729c044a2ef79c5
joilson <joilson_rg@hotmail.com>
2020-11-17 08:36:25 -0300

    chore: get names of functions of a class

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var props = [];
+var obj = myObj;
+do {
+    props = props.concat(Object.getOwnPropertyNames(obj));
+} while (obj = Object.getPrototypeOf(obj));
+
+const propss = props.sort().filter(function(e, i, arr) { 
+    if (e!=arr[i+1] && typeof myObj[e] == 'function') return true;
+});
