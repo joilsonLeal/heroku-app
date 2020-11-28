56d86ca6ce975891b351673d20e5b2f9a744301e
joilson <joilson_rg@hotmail.com>
2020-11-28 15:33:55 -0300

    ref: add method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-var props = [];
-var obj = myObj;
-do {
-    props = props.concat(Object.getOwnPropertyNames(obj));
-} while (obj = Object.getPrototypeOf(obj));
-
-const propss = props.sort().filter(function(e, i, arr) { 
-    if (e!=arr[i+1] && typeof myObj[e] == 'function') return true;
-});
+function getMethods(obj) {
+    var props = [];
+    var obj = myObj;
+    do {
+        props = props.concat(Object.getOwnPropertyNames(obj));
+    } while (obj = Object.getPrototypeOf(obj));
+    
+    const propss = props.sort().filter(function(e, i, arr) { 
+        if (e!=arr[i+1] && typeof myObj[e] == 'function') return true;
+    });
+}
